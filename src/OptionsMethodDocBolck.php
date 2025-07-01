f337dd1b6b25932c7de150113fe43d006a032f0e
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-07-01 23:40:00 +0900

    Check if docComment is a string in OptionsMethodDocBlock; update psalm.xml to remove suppressions for PossiblyNullArgument and PossiblyNullArrayOffset

--- src/OptionsMethodDocBolck.php
+++ src/OptionsMethodDocBolck.php
@@ -7,6 +7,7 @@ namespace BEAR\Resource;
 use phpDocumentor\Reflection\DocBlock\Tags\Param;
 use phpDocumentor\Reflection\DocBlockFactory;
 use ReflectionMethod;
+use function is_string;
 
 final class OptionsMethodDocBolck
 {
@@ -19,7 +20,7 @@ final class OptionsMethodDocBolck
     {
         $docComment = $method->getDocComment();
         $doc = $paramDoc = [];
-        if ($docComment) {
+        if (is_string($docComment)) {
             [$doc, $paramDoc] = $this->docBlock($docComment);
         }
 
